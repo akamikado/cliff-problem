0fd9bda2c332f837b03edf3e9b4eae6babd61865
akamikado <manishvasireddy1234@gmail.com>
2024-10-26 20:40:06 +0530

    feat: finished creating all q learning algos

---
+++ brain.py
@@ -0,0 +1,188 @@
+import numpy as np
+import random
+
+class QLearner:
+    def __init__(self, env, alpha=0.1, gamma=0.5, epsilon=0.1, epsilon_decay=0.99):
+        self.env = env
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.q1 = np.zeros((env.rows, env.cols, len(env.get_actions())))
+
+    def epsilon_greedy(self, state):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(range(len(self.env.get_actions())))
+        else:
+            return np.argmax(self.q1[state])
+
+    def update_q_value(self, state, next_state, action, reward):
+        self.q1[state][action] += self.alpha * (reward + self.gamma * np.max(self.q1[next_state]) - self.q1[state][action])
+
+    def train(self, episodes):
+        rewards = []
+
+        for i in range(episodes):
+            state = self.env.reset()
+            total_reward = 0
+            done = False
+            while not done:
+                action = self.epsilon_greedy(state)
+                next_state, reward, done = self.env.step(action)
+                self.update_q_value(state, next_state, action, reward)
+                state = next_state
+                total_reward += reward
+
+            rewards.append(total_reward)
+            total_reward = 0
+
+            self.epsilon *= self.epsilon_decay
+
+            if i % 500 == 0:
+                print(f"Episode {i} completed.")
+
+        return rewards, [self.q1]
+
+class DoubleQLearner(QLearner):
+    def __init__(self, env, alpha=0.1, gamma=0.5, epsilon=0.1, epsilon_decay=0.99):
+        super().__init__(env, alpha, gamma, epsilon, epsilon_decay)
+        self.q2 = np.zeros((env.rows, env.cols, len(env.get_actions())))
+
+    def epsilon_greedy(self, state):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(range(len(self.env.get_actions())))
+        else:
+            q = self.q1[state] + self.q2[state]
+            return np.argmax(q)
+
+    def update_q_value(self, state, next_state, action, reward):
+        if np.random.rand() < 0.5:
+            self.q1[state][action] += self.alpha * (reward + self.gamma * self.q2[next_state][np.argmax(self.q1[next_state])] - self.q1[state][action])
+        else:
+            self.q2[state][action] += self.alpha * (reward + self.gamma * self.q1[next_state][np.argmax(self.q2[next_state])] - self.q2[state][action])
+
+    def train(self, episodes):
+        rewards = []
+
+        for i in range(episodes):
+            state = self.env.reset()
+            total_reward = 0
+            done = False
+            while not done:
+                action = self.epsilon_greedy(state)
+                next_state, reward, done = self.env.step(action)
+                self.update_q_value(state, next_state, action, reward)
+                state = next_state
+                total_reward += reward
+
+            rewards.append(total_reward)
+            total_reward = 0
+
+            self.epsilon *= self.epsilon_decay
+
+            if i % 500 == 0:
+                print(f"Episode {i} completed.")
+
+        return rewards, [self.q1, self.q2]
+
+class TripleQLearner(DoubleQLearner):
+    def __init__(self, env, alpha=0.1, gamma=0.5, epsilon=0.1, epsilon_decay=0.99):
+        super().__init__(env, alpha, gamma, epsilon, epsilon_decay)
+        self.q3 = np.zeros((env.rows, env.cols, len(env.get_actions())))
+
+    def epsilon_greedy(self, state):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(range(len(self.env.get_actions())))
+        else:
+            q = self.q1[state] + self.q2[state] + self.q3[state]
+            return np.argmax(q)
+
+    def update_q_value(self, state, next_state, action, reward):
+        choice = random.choice(range(3))
+
+        if choice == 0:
+            best_action = np.argmax((self.q2[next_state] + self.q3[next_state]) / 2)
+            self.q1[state][action] += self.alpha * (reward + self.gamma * min(self.q2[next_state][best_action], self.q3[next_state][best_action]) - self.q1[state][action])
+        elif choice == 1:
+            best_action = np.argmax((self.q1[next_state] + self.q3[next_state]) / 2)
+            self.q2[state][action] += self.alpha * (reward + self.gamma * min(self.q1[next_state][best_action], self.q3[next_state][best_action]) - self.q2[state][action])
+        else:
+            best_action = np.argmax((self.q2[next_state] + self.q1[next_state]) / 2)
+            self.q3[state][action] += self.alpha * (reward + self.gamma * self.q1[next_state][np.argmax(self.q3[next_state])] - self.q3[state][action])
+            self.q3[state][action] += self.alpha * (reward + self.gamma * min(self.q1[next_state][best_action], self.q2[next_state][best_action]) - self.q3[state][action])
+
+    def train(self, episodes):
+        rewards = []
+
+        for i in range(episodes):
+            state = self.env.reset()
+            total_reward = 0
+            done = False
+            while not done:
+                action = self.epsilon_greedy(state)
+                next_state, reward, done = self.env.step(action)
+                self.update_q_value(state, next_state, action, reward)
+                state = next_state
+                total_reward += reward
+
+            rewards.append(total_reward)
+            total_reward = 0
+
+            self.epsilon *= self.epsilon_decay
+
+            if i % 500 == 0:
+                print(f"Episode {i} completed.")
+
+        return rewards, [self.q1, self.q2, self.q3]
+
+class QuadrupleQLearner(TripleQLearner):
+    def __init__(self, env, alpha=0.1, gamma=0.5, epsilon=0.1, epsilon_decay=0.99):
+        super().__init__(env, alpha, gamma, epsilon, epsilon_decay)
+        self.q4 = np.zeros((env.rows, env.cols, len(env.get_actions())))
+
+    def epsilon_greedy(self, state):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(range(len(self.env.get_actions())))
+        else:
+            q = self.q1[state] + self.q2[state] + self.q3[state] + self.q4[state]
+            return np.argmax(q)
+
+    def update_q_value(self, state, next_state, action, reward):
+        choice = random.choice(range(4))
+        if choice == 0:
+            best_action = np.argmax((self.q2[next_state] + self.q3[next_state] + self.q4[next_state]) / 3)
+            self.q1[state][action] += self.alpha * (reward + self.gamma * self.q2[next_state][np.argmax(self.q1[next_state])] - self.q1[state][action])
+            self.q1[state][action] += self.alpha * (reward + self.gamma * min(self.q2[next_state][best_action], self.q3[next_state][best_action], self.q4[next_state][best_action]) - self.q1[state][action])
+        elif choice == 1:
+            best_action = np.argmax((self.q1[next_state] + self.q3[next_state] + self.q4[next_state]) / 3)
+            self.q2[state][action] += self.alpha * (reward + self.gamma * min(self.q1[next_state][best_action], self.q3[next_state][best_action], self.q4[next_state][best_action]) - self.q2[state][action])
+        elif choice == 2:
+            best_action = np.argmax((self.q1[next_state] + self.q2[next_state] + self.q4[next_state]) / 3)
+            self.q3[state][action] += self.alpha * (reward + self.gamma * min(self.q1[next_state][best_action], self.q2[next_state][best_action], self.q4[next_state][best_action]) - self.q3[state][action])
+        else:
+            best_action = np.argmax((self.q1[next_state] + self.q2[next_state] + self.q3[next_state]) / 3)
+            self.q4[state][action] += self.alpha * (reward + self.gamma * min(self.q1[next_state][best_action], self.q2[next_state][best_action], self.q3[next_state][best_action]) - self.q4[state][action])
+
+    def train(self, episodes):
+        rewards = []
+
+        for i in range(episodes):
+            state = self.env.reset()
+            total_reward = 0
+            done = False
+            while not done:
+                action = self.epsilon_greedy(state)
+                next_state, reward, done = self.env.step(action)
+                self.update_q_value(state, next_state, action, reward)
+                state = next_state
+                total_reward += reward
+
+            rewards.append(total_reward)
+            total_reward = 0
+
+            self.epsilon *= self.epsilon_decay
+
+            if i % 500 == 0:
+                print(f"Episode {i} completed.")
+
+        return rewards, [self.q1, self.q2, self.q3, self.q4]
--- env.py
+++ env.py
@@ -8,27 +8,30 @@ class Model:
     left = {'x': 0, 'y': -1}
     right = {'x': 0, 'y': 1}
 
-    def __init__(self, X=50, Y=30):
-        self.Y = Y
-        self.X = X
-        self.grid = np.zeroes((X, Y))
+    def __init__(self, cols=50, rows=30):
+        self.rows = rows
+        self.cols = cols
+        self.grid = np.zeroes((cols, rows))
         self.bottom_cliff = set()
         self.top_cliff = set()
-        for i in range(2, X-2):
-            self.top_cliff.add((i, 0))
-            self.bottom_cliff.add((i, Y-1))
-        self.goal = random.choice([(i, j) for j in range(Y) for i in range(X-2, X)])
-        self.agent_pos = (0, Y//2)
+        for i in range(2, cols-2):
+            self.bottom_cliff.add((i, 0))
+            self.top_cliff.add((i, rows-1))
+        self.goal = random.choice([(i, j) for j in range(rows) for i in range(cols-2, cols)])
+        self.agent_pos = (0, rows//2)
         self.agent_dist = math.sqrt((self.agent_pos[0]-self.goal[0])**2 + (self.agent_pos[1]-self.goal[1])**2)
 
-    def move(self, action):
-        new_pos = (self.agent[0] + action['x'], self.agent[1] + action['y'])
+    def get_actions(self):
+        return [self.up, self.down, self.left, self.right]
+
+    def step(self, action):
+        new_pos = (self.agent_pos[0] + action['x'], self.agent_pos[1] + action['y'])
         will_slip = random.random() < 0.3
 
         if will_slip:
-            new_pos = (self.agent[0], self.agent[1] + random.choice([-1, 1]))
+            new_pos = (self.agent_pos[0], self.agent_pos[1] + random.choice([-1, 1]))
 
-        if new_pos[0] < 0 or new_pos[0] >= self.X or new_pos[1] < 0 or new_pos[1] >= self.Y:
+        if new_pos[0] < 0 or new_pos[0] >= self.cols or new_pos[1] < 0 or new_pos[1] >= self.rows:
             reward = -100
             done = True
         elif new_pos in self.top_cliff or new_pos in self.bottom_cliff:
@@ -45,16 +48,16 @@ class Model:
             reward = -1 if new_pos_dist > self.agent_dist else 1
             done = False
 
-        self.agent = new_pos
+        self.agent_pos = new_pos
 
-        return reward, done
+        return self.agent_pos, reward, done
 
     def is_path_blocked(self):
         path_blocked = True
-        if self.agent[0] >= self.X-3:
+        if self.agent_pos[0] >= self.cols-3:
             return False
-        for i in range(self.agent[0]+1, self.X-3):
-            for j in range(self.Y):
+        for i in range(self.agent_pos[0]+1, self.cols-3):
+            for j in range(self.rows):
                 if (i, j) in self.top_cliff or (i, j) in self.bottom_cliff:
                     path_blocked = True
                 else:
@@ -65,32 +68,26 @@ class Model:
         return False
 
     def grow_cliff(self):
-        if self.agent[0] >= self.X-3:
+        if self.agent_pos[0] >= self.cols-3:
             return
         top_possible_boxes = []
         bottom_possible_boxes = []
-        for i in range(self.agent[0]+1, self.X-3):
-            for j in range(self.Y):
-                if (i, j) in self.top_cliff and (i, j+1) not in self.top_cliff:
-                    top_possible_boxes.append((i, j+1))
-                elif (i, j) in self.bottom_cliff and (i, j-1) not in self.bottom_cliff:
-                    bottom_possible_boxes.append((i, j-1))
+        for i in range(self.agent_pos[0]+1, self.cols-3):
+            for j in range(self.rows):
+                if (i, j) in self.top_cliff and (i, j-1) not in self.top_cliff:
+                    top_possible_boxes.append((i, j-1))
+                elif (i, j) in self.bottom_cliff and (i, j+1) not in self.bottom_cliff:
+                    bottom_possible_boxes.append((i, j+1))
 
         self.top_cliff.add(random.choice(top_possible_boxes))
         self.bottom_cliff.add(random.choice(bottom_possible_boxes))
 
-    def move_goal(self):
-        if self.agent[0] >= self.X-3:
-            return
-        self.goal = random.choice([(i, j) for j in range(self.Y) for i in range(self.X-2, self.X)])
-        self.agent_dist = math.sqrt((self.agent[0]-self.goal[0])**2 + (self.agent[1]-self.goal[1])**2)
-
     def reset(self):
-        self.goal = random.choice([(i, j) for j in range(self.Y) for i in range(self.X-2, self.X)])
-        self.agent_pos = (0, self.Y//2)
+        self.goal = random.choice([(i, j) for j in range(self.rows) for i in range(self.cols-2, self.cols)])
+        self.agent_pos = (0, self.rows//2)
         self.agent_dist = math.sqrt((self.agent_pos[0]-self.goal[0])**2 + (self.agent_pos[1]-self.goal[1])**2)
         self.top_cliff = set()
         self.bottom_cliff = set()
-        for i in range(2, self.X-2):
-            self.top_cliff.add((i, 0))
-            self.bottom_cliff.add((i, self.Y-1))
+        for i in range(2, self.cols-2):
+            self.bottom_cliff.add((i, 0))
+            self.top_cliff.add((i, self.rows-1))
